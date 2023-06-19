0ba833cc0109da62dec2df3081a51e2c3ed15eea
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 15:58:27 +0300

    plural

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+
+
+function pluralizing(words) {
+  const pluralForms = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+ 
+    {
+      pluralForms.push(word + "s");
+    }
+  }
+
+  return pluralForms;
+}
+
+const singular = ["cat", "cat", "dog", "mouse", ];
+const plural = pluralizing(singular);
+console.log(plural);
